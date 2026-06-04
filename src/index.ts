c7993a43bbceec40a99e7f442408d482c4d2a7af
Trumaine <trumaine@rogers.com>
2026-06-04 09:53:55 -0400

    chapter 1, lesson 6: Fileservers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080);
+app.use(express.static("."));
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
