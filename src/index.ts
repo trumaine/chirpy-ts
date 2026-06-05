4fda5fca5d100ed54bb7fefd5f67298bd1aa0508
Trumaine <trumaine@rogers.com>
2026-06-05 07:31:30 -0400

    chapter 1, lesson: 12: Custom Handlers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
