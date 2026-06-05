89dc69c50ef1835dd4124459414348fff29603ba
Trumaine <trumaine@rogers.com>
2026-06-05 16:12:44 -0400

    chapter 3, lesson 4: Admin Namespace

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -2,5 +2,15 @@ import { Request, Response } from "express";
 import { config } from "../config.js";
 
 export async function handlerMetrics(_: Request, res: Response) {
-    res.send(`Hits: ${config.fileserverHits}`);
+    res.header({
+        "Content-Type": "text/html",
+    });
+    res.statusCode = 200;
+    const html = `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`;
+    res.send(html);
 }
--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
-app.get("/api/metrics", handlerMetrics);
-app.get("/api/reset", handlerReset);
+
+app.get("/admin/metrics", handlerMetrics);
+app.get("/admin/reset", handlerReset);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
