585d06c80c83935e038b72be4ab5d849f5639262
Trumaine <trumaine@rogers.com>
2026-06-09 08:47:28 -0400

    chapter 6, lesson 2 & 3: Drizzle ORM & Queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
+}
+
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Environment variable ${key} is not set`);
+    }
+    return value;
 }
 
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
